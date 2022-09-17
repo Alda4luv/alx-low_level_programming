b95a3c2958a0c23efc0943ecb723688ab6e3e115
mirian <momentum4luv@gmail.com>
2022-09-16 21:06:27 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,18 +10,21 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a >- b && b >- c)
-{
-largest = a;
+    int largest;
+
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
 }
-else if (b >- a && a >- c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
-}
+
